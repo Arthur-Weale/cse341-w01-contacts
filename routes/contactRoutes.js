3f7670f16f8e0acc306db4746c126acdca6c30b1
Arthur Weale <ajayrossy340@gmail.com>
2025-05-10 17:02:23 +0200

    Added try block , api working

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,11 +1,31 @@
 const express = require("express")
-const  {getAllContacts} = require("../controller/contact")
+const  {getAllContacts, getContact} = require("../controller/contact")
 const router = express.Router()
+const { ObjectId } = require("mongodb")
 
 router.get("/getAll", async (req, res)=>{
-    const response = await getAllContacts();
+    try{
+        const response = await getAllContacts();
     console.log(response)
     res.status(200).json(response)
+    console.log("Request successful")
+    }catch(err){
+        console.log("Error fetching contacts ", err.message)
+    }
+    
+})
+
+router.get("/get", async(req, res) => {
+    try{
+        const id = "681e8fdf41ec986c6cfbaa52"
+    const object_id = new ObjectId(id)
+    const response = await getContact(object_id)
+    res.status(200).json(response)
+        console.log("Contact Request successful")
+    }catch(err){
+        console.log("Contact request failed", err.message)
+    }
+    
 })
 
 
