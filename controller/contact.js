67aa446b0154bd14276bbbc15eb125fa4958f004
Arthur Weale <ajayrossy340@gmail.com>
2025-05-10 16:58:57 +0200

    Added get for one contact

--- controller/contact.js
+++ controller/contact.js
@@ -1,4 +1,6 @@
+const { ObjectId } = require("mongodb");
 const {initdb} = require("../model/connectdb")
+
 
 
 const getAllContacts = async () => {
@@ -14,4 +16,18 @@ const getAllContacts = async () => {
     }
 }
 
-module.exports = {getAllContacts};
+const getContact = async () => {
+    try{
+        const client = await initdb()
+        const result = await client.db("contactDB").collection("contacts").findOne({"_id":  new ObjectId("681e8f9f41ec986c6cfbaa4c")})
+        console.log(result)
+        return result
+    }catch(err){
+        console.log("Error fetching contacts:", err.message)
+        process.exit(1)
+    }
+}
+
+module.exports = {
+    getAllContacts,
+    getContact};
